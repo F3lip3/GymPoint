403b6682da4d153e23bcd258d0e422809076b8b9
Felipe H. Teixeira <felipe@dinfo.com.br>
2019-11-17 02:06:53 -0300

    Created sign in page style

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="GymPoint" />
+      <form autoComplete="off">
+        <label htmlFor="email">
+          SEU E-MAIL
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="exemplo@email.com"
+          />
+        </label>
+        <label htmlFor="password">
+          SUA SENHA
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+          />
+        </label>
+        <button type="submit">Entrar no Sistema</button>
+      </form>
+    </>
+  );
 }
--- web/src/pages/_layouts/auth/styles.js
+++ web/src/pages/_layouts/auth/styles.js
@@ -3,4 +3,68 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   background-color: #ee4d64;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 360px;
+  text-align: center;
+  border: 0;
+  border-radius: 4px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  background: #fff;
+  padding: 50px 30px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    label {
+      font-family: Roboto;
+      font-size: 14px;
+      font-weight: bold;
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+      margin-bottom: 15px;
+      color: #444;
+    }
+
+    input {
+      height: 45px;
+      border-radius: 4px;
+      border: solid 1px #ddd;
+      background-color: #fff !important;
+      color: #333 !important;
+      margin-top: 5px;
+      padding: 0 15px;
+
+      &::placeholder {
+        color: #999;
+      }
+
+      &:-webkit-autofill,
+      &:-webkit-autofill:hover,
+      &:-webkit-autofill:focus {
+        background-color: #fff !important;
+        color: #333 !important;
+      }
+    }
+
+    button {
+      height: 45px;
+      border: 0;
+      border-radius: 4px;
+      background-color: #ee4d64;
+      font-family: Roboto;
+      font-size: 16px;
+      font-weight: bold;
+      color: #fff;
+    }
+  }
 `;
