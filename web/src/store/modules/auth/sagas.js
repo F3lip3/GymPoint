cfe4f6d7bbabb6162cb7beb4dbbccb4e59e67bfb
Felipe H. Teixeira <felipe@dinfo.com.br>
2019-11-18 22:06:09 -0300

    Create error handler with toastify and setToken

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -13,11 +14,24 @@ export function* signIn({ payload }) {
       password
     });
     const { token, user } = response.data;
+    api.defaults.headers.Authorization = `Bearer ${token}`;
     yield put(signInSuccess(token, user));
     history.push('/students');
   } catch (err) {
+    toast.error('Usuário ou senha inválidos!');
     yield put(signInFailure());
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function setToken({ payload }) {
+  if (!payload) return;
+  const { token } = payload.auth;
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn)
+]);
