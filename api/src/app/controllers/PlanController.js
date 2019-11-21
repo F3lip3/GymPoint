da67bdf2f93856770f151eb60571f8a4f25dcaa4
Felipe H. Teixeira <felipe@dinfo.com.br>
2019-11-20 23:58:24 -0300

    Updated plans index to sort by duration

--- api/src/app/controllers/PlanController.js
+++ api/src/app/controllers/PlanController.js
@@ -3,7 +3,9 @@ import Plan from '../models/Plan';
 
 class PlanController {
   async index(req, res) {
-    const plans = await Plan.findAll();
+    const plans = await Plan.findAll({
+      order: [['duration', 'ASC']]
+    });
     return res.json(plans);
   }
 
