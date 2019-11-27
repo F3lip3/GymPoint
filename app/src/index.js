f39df15fd0d8ed54fa62744632634cf5aee46046
Felipe H. Teixeira <felipe@dinfo.com.br>
2019-11-26 23:06:28 -0300

    Create mobile app base structure

---
+++ app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+import './config/ReactotronConfig';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Welcome</Text>
+    </SafeAreaView>
+  );
+}
